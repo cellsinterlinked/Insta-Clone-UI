d3240e0b3cadea2aea722f46e98734724f354014
Scott <scott_hoffmann1879@outlook.com>
2021-06-24 16:22:23 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import './App.css';
+import Landing from './Pages/Landing';
+import Post from './Pages/Post';
 
 function App() {
+
+  let users = [
+    {
+      name: 'Scott_Billings',
+      picture: "https://res.cloudinary.com/dbnapmpvm/image/upload/v1624483656/ikkm4fmdyxoxxlm5zxte.jpg"
+    },
+    {
+      name: 'Lauren',
+      picture: 'https://res.cloudinary.com/dbnapmpvm/image/upload/v1624483784/aim6f28z0aeidsbqwlit.jpg'
+    },
+    {
+      name: 'Ken_Montgomery',
+      picture: 'https://res.cloudinary.com/dbnapmpvm/image/upload/v1624500899/niha1kliyko28sd4e4mn.jpg'
+    }
+  ]
+
+  let routes;
+
+  routes = (
+    <Switch>
+      <Route path="/home">
+        <Landing users={users} />
+      </Route>
+
+      <Route path="/post">
+        <Post />
+      </Route>
+      <Redirect to="/home" />
+
+    </Switch>
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="header-wrapper">
+        <h2 className='title-head'>Nonurgentgram</h2>
+      </div>
+      <main>{routes}</main> 
+    </Router>
   );
 }
 
